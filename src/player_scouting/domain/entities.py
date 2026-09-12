211ecd593f7f4e3b7662ca8c3aba17d056189334
Alberto Quesada <albertoqv19@gmail.com>
2026-09-12 17:39:39 +0200

    feat: valida que player_id sea un entero mayor que 0

--- src/player_scouting/domain/entities.py
+++ src/player_scouting/domain/entities.py
@@ -1,5 +1,7 @@
 class Jugador:
     def __init__(self,player_id,nombre,posicion,fecha_nacimiento):
+        if not isinstance(player_id, int):
+            raise ValueError("El player_id debe ser un entero")
         if player_id <= 0:
             raise ValueError("El player_id debe ser un valor mayor que 0")
         self.player_id = player_id
