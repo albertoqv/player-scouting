6755995f5847d015763c83522d12458299c57c3b
Alberto Quesada <albertoqv19@gmail.com>
2026-09-12 18:13:00 +0200

    feat: comparacion guarda sus valores correctamente

---
+++ src/player_scouting/domain/comparacion.py
@@ -0,0 +1,5 @@
+class Comparacion:
+    def __init__(self,jugador1,jugador2,puntuacion_similitud):
+        self.jugador1 = jugador1
+        self.jugador2 = jugador2
+        self.puntuacion_similitud = puntuacion_similitud
