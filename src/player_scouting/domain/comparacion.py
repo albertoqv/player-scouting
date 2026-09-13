83a658414bdb8d8c16cfab0878bab03bdea14fe7
Alberto Quesada <albertoqv19@gmail.com>
2026-09-13 10:22:09 +0200

    feat: implementa igualdad simétrica en Comparacion

--- src/player_scouting/domain/comparacion.py
+++ src/player_scouting/domain/comparacion.py
@@ -3,3 +3,9 @@ class Comparacion:
         self.jugador1 = jugador1
         self.jugador2 = jugador2
         self.puntuacion_similitud = puntuacion_similitud
+        
+    
+    def __eq__(self,other):
+        mismo_orden = self.jugador1 == other.jugador1 and self.jugador2 == other.jugador2
+        orden_invertido = self.jugador1 == other.jugador2 and self.jugador2 == other.jugador1
+        return (mismo_orden or orden_invertido) and self.puntuacion_similitud == other.puntuacion_similitud
