2cda8be9e40db20ca57d532cc252e86c4c90f1c9
Alberto Quesada <albertoqv19@gmail.com>
2026-09-13 10:17:05 +0200

    test: dos comparaciones de los mismos jugadores son iguales

--- tests/domain/test_comparacion.py
+++ tests/domain/test_comparacion.py
@@ -13,4 +13,13 @@ def test_comparacion_guarda_sus_datos_correctamente():
     assert resultado.jugador1 == Jugador(75, "Leo", "Delantero", date(2003, 6, 5))
     assert resultado.jugador2 == Jugador(80, "Kylian", "Delantero", date(1998, 12, 20))
     assert resultado.puntuacion_similitud == PuntuacionSimilitud(75)
-    
+
+def test_dos_comparaciones_con_los_mismos_jugadores_son_iguales():
+    jugador1 = Jugador(75,"Pepe","delantero",date(2003, 6, 5))
+    jugador2 = Jugador(80, "Kylian", "Delantero", date(1998, 12, 20))
+    puntuacion_similitud = PuntuacionSimilitud(75)
+    a = Comparacion(jugador1,jugador2,puntuacion_similitud)
+    b = Comparacion(jugador2,jugador1,puntuacion_similitud)
+
+
+    assert a == b
