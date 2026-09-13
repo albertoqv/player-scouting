e56ffb43cf614e775c700483b49fea36dc38d25a
Alberto Quesada <albertoqv19@gmail.com>
2026-09-13 20:15:06 +0200

    feat: implementa la fórmula de similitud para valores distintos

--- src/player_scouting/domain/calculador_similitud.py
+++ src/player_scouting/domain/calculador_similitud.py
@@ -2,4 +2,6 @@ class CalculadorDeSimilitud:
     def similitud_metrica(self,valor_a,valor_b):
         if valor_a == valor_b:
             return 1
-        
+        else:
+            resultado = 1 - abs(valor_a - valor_b)/max(valor_a,valor_b)
+            return resultado
