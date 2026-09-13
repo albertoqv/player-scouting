8b7b5557d5a681783c1948728999b19a9bc0e504
Alberto Quesada <albertoqv19@gmail.com>
2026-09-13 20:35:11 +0200

    feat: implementa similitud_total combinando goles y asistencias

--- src/player_scouting/domain/calculador_similitud.py
+++ src/player_scouting/domain/calculador_similitud.py
@@ -1,3 +1,4 @@
+from player_scouting.domain.value_objects import PuntuacionSimilitud
 class CalculadorDeSimilitud:
     def similitud_metrica(self,valor_a,valor_b):
         if valor_a == valor_b:
@@ -5,3 +6,10 @@ class CalculadorDeSimilitud:
         else:
             resultado = 1 - abs(valor_a - valor_b)/max(valor_a,valor_b)
             return resultado
+    
+    def similitud_total(self,estadistica_j1,estadistica_j2):
+        similitud_goles = self.similitud_metrica(estadistica_j1.goles,estadistica_j2.goles)
+        similitud_asistencias =self.similitud_metrica(estadistica_j1.asistencias,estadistica_j2.asistencias)
+        media = (similitud_goles + similitud_asistencias) / 2
+        porcentaje = round(media *100)
+        return PuntuacionSimilitud(porcentaje)
