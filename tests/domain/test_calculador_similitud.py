ee3931d4a884281dca00e616562d457d051a962c
Alberto Quesada <albertoqv19@gmail.com>
2026-09-13 20:12:19 +0200

    test: comprueba la fórmula de similitud con valores distintos

--- tests/domain/test_calculador_similitud.py
+++ tests/domain/test_calculador_similitud.py
@@ -8,3 +8,8 @@ def test_similitud_metrica_con_valores_iguales_es_uno():
     resultado = calculador.similitud_metrica(10, 10)
 
     assert resultado == 1
+
+def test_calcular_similitud_metrica_con_valores_distintos():
+    calculador = CalculadorDeSimilitud()
+    resultado = calculador.similitud_metrica(10, 12)
+    assert resultado == pytest.approx(0.833, rel=0.01)
